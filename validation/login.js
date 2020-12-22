7c6c08cb626a64d3eba5a6e3d9b1fb95e04a3793
tom-cheung <wai.tomcheung@gmail.com>
2020-12-21 18:47:47 -0800

    connect to MERNdb

--- validation/login.js
+++ validation/login.js
@@ -4,11 +4,11 @@ const validText = require('./valid_text');
 module.exports = function validateLoginInput(data) { // looks to be a self defined method 
     let errors = {};
 
-    data.email = validText(data.email) ? data.email : ''; // checking if the email and password are valid string. Returns them if they're otherwise returns empty strings 
+    data.email = validText(data.email) ? data.email : ''; // checking if the email and password are valid string. Returns them if they're otherwise returns empty strings. Ternary statements 
     data.password = validText(data.password) ? data.password : ''; 
 
     if (!Validator.isEmail(data.email)) {
-        errors.email = "Email is invalid"; // if the data.email when passed into Validator.isEmail returns false then you set a key of emails in errors and assign an error message. isEmail must be a function of Validator (imported above)
+        errors.email = "Email is invalid"; // if the data.email when passed into Validator.isEmail returns false then you set a key of emails in errors and assign an error message. isEmail must be a function of Validator library (imported above)
     }
 
     if (Validator.isEmpty(data.email)) { // seems to do something similar to the above, just with a different error message 
@@ -20,7 +20,9 @@ module.exports = function validateLoginInput(data) { // looks to be a self defin
     }
 
     return {
-        errors, 
+        errors, // es6 synthax, could also be errors: errors
         isValid: Object.keys(errors).length === 0 // interesting. it's just returning a boolean if there are any error messages 
     };
 };
+
+// these libraries are just tools that make this whole process easier. You could accomplish this without this Validator library I believe. 
--- validation/register.js
+++ validation/register.js
@@ -4,11 +4,12 @@ const validText = require('./valid_text');
 module.exports = function validateRegisterInput(data) {
     let errors = {}; 
 
-    data.handle = validText(data.handle) ? data.handle : ''; 
+    data.handle = validText(data.handle) ? data.handle : ''; // ternary statement that checks if the data is validText, and returns it if it is, if not it returns an empty string 
     data.email = validText(data.email) ? data.email : '';
     data.password = validText(data.password) ? data.password : '';
     data.password2 = validText(data.password2) ? data.password2 : ''; 
     
+    // validations for handle 
     if(!Validator.isLength(data.handle, { min: 2, max: 30})) {
         errors.handle = 'Handle must be between 2 and 30 characters';
     }
@@ -17,6 +18,7 @@ module.exports = function validateRegisterInput(data) {
         errors.handle = 'Handle field is required';
     }
 
+    // validation for emails 
     if (Validator.isEmpty(data.email)) {
         errors.email = 'Email field is required';
     }
@@ -25,6 +27,7 @@ module.exports = function validateRegisterInput(data) {
         errors.email = 'Email is invalid';
     }
 
+    // validation for password 
     if(Validator.isEmpty(data.password)) {
         errors.password = 'Password field is required';
     }
